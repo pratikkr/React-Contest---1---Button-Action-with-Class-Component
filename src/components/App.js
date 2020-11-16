ea69f0375f8b062db466ff3835396b3aa2a2f67e
pratik kumar <kumarpratik861@gmail.com>
2020-11-17 01:38:54 +0530

    done assignment button works

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Para from "./Para";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return (
+      <div id="main">
+        <button
+          id="click"
+          onClick={() => this.setState({ show: !this.state.show })}
+        >
+          click
+        </button>
+        {this.state.show ? <Para /> : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
